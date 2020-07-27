a60f3b6087b762628fc106a0c6eaacae1c0b788c
Siva <56733505+BOT-CODER@users.noreply.github.com>
2020-07-27 01:35:51 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-echo -e "                \e[31;1mInstalling Cam_U\e[0m"
-apt-get -y update 
+echo -e "                \e[31;1mInstalling Cam_U\e[0m" 
 apt-get -y install python 
 apt-get -y install python2 
 apt-get -y install php 
-apt-get -y install unzip 
-apt-get -y install wget 
 clear
 echo -e "\e[1;92m[*]Cam_U is installed To run CamU type '\e[93mpython3 CamU.py'\e[0m "
